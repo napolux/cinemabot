2fc96dcc8368b9115234ac75d01a04394f4a7c7b
Francesco Napoletano <napolux@gmail.com>
2016-09-24 14:46:45 +0200

    Adding sendMessage()

--- index.js
+++ index.js
@@ -23,3 +23,34 @@ app.get('/webhook', function (req, res) {
         res.send('Token non valido');
     }
 });
+
+// Gestione degli eventi
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Messaggio inviato: " + event.message.text});
+        }
+    }
+    res.sendStatus(200);
+});
+
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.7/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Errore nella spedizione del messaggio: ', error);
+        } else if (response.body.error) {
+            console.log('Errore: ', response.body.error);
+        }
+    });
+};
