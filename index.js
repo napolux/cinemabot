a68bff981ef59c94a8cbeb05d9740b0dd06a4d3d
Francesco Napoletano <napolux@gmail.com>
2016-09-24 12:32:30 +0200

    First commit for index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// Inizializziamo i componenti dell'applicazione
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// L'applicazione sarà in ascolto sulla porta 3000 o su una porta predefinita dal server
+app.listen((process.env.PORT || 3000));
+
+// Homepage
+app.get('/', function (req, res) {
+    res.send('Questo è cinemabot');
+});
